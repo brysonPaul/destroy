a3be2489c50dfbff712e9af81b5196c5a5cfa8d5
brysonPaul <brysonpaul1870@gmail.com>
2022-02-22 17:58:06 -0500

    9.6 100k

--- src/main/java/destroy.java
+++ src/main/java/destroy.java
@@ -1,35 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.util.StringTokenizer;
+
 //Bryson Paul       2/02/2022       Arup Guha COP3503 Destroy Solution
 public class destroy {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        djSet computers = new djSet(sc.nextInt());
-        String[] currentConnections = new String[sc.nextInt()];
-        int[] indexesOfConnectionsDestroyed = new int[sc.nextInt()];
+    public static void main(String[] args) throws IOException {
+        BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(r.readLine());
+        djSet computers = new djSet(Integer.parseInt(st.nextToken()));
+        String[] currentConnections = new String[Integer.parseInt(st.nextToken())];
+        String[] connectionsDestroyed = new String[Integer.parseInt(st.nextToken())];
         //finalOutputs is a long to make sure our values cannot go over
-        long[] finalOutputs = new long[indexesOfConnectionsDestroyed.length + 1];
+        long[] finalOutputs = new long[connectionsDestroyed.length + 1];
         //takes in both integers for the connection line
         for (int x = 0; x < currentConnections.length; x++) {
-            String hold = sc.next() + " " + sc.next();
-            currentConnections[x] = hold;
+            currentConnections[x] = r.readLine();
         }
         //marks indices which will be destroyed
-        for (int x = 0; x < indexesOfConnectionsDestroyed.length; x++) {
-            int hold = sc.nextInt() - 1;
-            indexesOfConnectionsDestroyed[x] = hold;
+        for (int x = 0; x < connectionsDestroyed.length; x++) {
+            int hold = Integer.parseInt(r.readLine()) - 1;
+            connectionsDestroyed[x] = currentConnections[hold];
+            currentConnections[hold] = null;
         }
         //adds rest of values to dj set, and disregards marked ones from the djSet
         for (int x = 0; x < currentConnections.length; x++) {
-            int hold = contains(x, indexesOfConnectionsDestroyed);
-            if (hold == -1) {
+            if (currentConnections[x]!=null) {
                 computers.union(currentConnections[x]);
             }
         }
         //adds current severed version to our finaloutputs
         finalOutputs[finalOutputs.length - 1] = computers.findConnectivity();
-        for (int x = indexesOfConnectionsDestroyed.length - 1; x >= 0; x--) {
+        for (int x = connectionsDestroyed.length - 1; x >= 0; x--) {
             //starts adding connections from the back to the front, and then puts them into the array
-            computers.union(currentConnections[indexesOfConnectionsDestroyed[x]]);
+            computers.union(connectionsDestroyed[x]);
             finalOutputs[x] = computers.findConnectivity();//adds it to end
         }
         //because everything was added from the back-up, I can loop through normally and print out everything in "order"
@@ -38,27 +43,20 @@ public class destroy {
         }
 
     }
-
-    //sees if the value is in the arr
-    public static int contains(int value, int[] arr) {
-        for (int x = 0; x < arr.length; x++) {
-            if (value == arr[x]) {
-                return x;
-            }
-        }
-        return -1;
-    }
 }
 
 class djSet {
     long[] set;
-
+    long[] size;
     public djSet(int x) {
         set = new long[x + 1];
+        size = new long[x + 1];
         //makes each set at least contain itself
         for (long i = 0; i < set.length; i++) {
             set[(int)i] = i;
+            size[(int)i] = 1;
         }
+        size[0]=0;
     }
 
     //adds string of two sets to each other
@@ -72,8 +70,10 @@ class djSet {
 
         if (root1 == root2) return;
 
-        // Attach tree of v2 to tree of v1.
+        // Attach tree of v2 to tree of v1, then add the size of root2 into the size array of root1
         set[(int) root2] = root1;
+        size[(int) root1] += size[(int)root2];
+        size[(int)root2] = 0;
     }
 
     //returns the root of the set working with, and does path compression on the way out for speed ups later
@@ -91,16 +91,8 @@ class djSet {
     //finds the connectivity by going through the array and groups similar roots together. At end squares each similar root total and sums it
     public long findConnectivity() {
         long sum = 0;
-        long[] arr = new long[set.length];
-        //we have to start at one because we technically make a zero'th set, thus offsetting each number by one
-        for (int x = 1; x < arr.length; x++) {
-            arr[(int)findSet(set[x])] += 1;
-        }
-        for (int x = 0; x < arr.length; x++) {
-            if (arr[x] == 0) {
-                continue;
-            }
-            sum += arr[x] * arr[x];
+        for(int x=0;x<size.length;x++){
+            sum+=(size[x]*size[x]);
         }
         return sum;
     }
