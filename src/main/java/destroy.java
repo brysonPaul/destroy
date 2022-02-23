82d347c49e546f098d937caea58842be1f77079d
brysonPaul <brysonpaul1870@gmail.com>
2022-02-23 13:27:45 -0500

    1.1 100k :)

--- src/main/java/destroy.java
+++ src/main/java/destroy.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.StringTokenizer;
 
 //Bryson Paul       2/02/2022       Arup Guha COP3503 Destroy Solution
@@ -9,13 +12,14 @@ public class destroy {
         BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(r.readLine());
         djSet computers = new djSet(Integer.parseInt(st.nextToken()));
-        String[] currentConnections = new String[Integer.parseInt(st.nextToken())];
-        String[] connectionsDestroyed = new String[Integer.parseInt(st.nextToken())];
+        connection[] currentConnections = new connection[Integer.parseInt(st.nextToken())];
+        connection[] connectionsDestroyed = new connection[Integer.parseInt(st.nextToken())];
         //finalOutputs is a long to make sure our values cannot go over
         long[] finalOutputs = new long[connectionsDestroyed.length + 1];
         //takes in both integers for the connection line
         for (int x = 0; x < currentConnections.length; x++) {
-            currentConnections[x] = r.readLine();
+            st = new StringTokenizer(r.readLine());
+            currentConnections[x] = new connection(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         }
         //marks indices which will be destroyed
         for (int x = 0; x < connectionsDestroyed.length; x++) {
@@ -44,13 +48,25 @@ public class destroy {
     }
 }
 
+class connection {
+    int c1;
+    int c2;
+
+    public connection(int c1, int c2) {
+        this.c1 = c1;
+        this.c2 = c2;
+    }
+}
+
 class djSet {
     int[] set;
     int[] size;
+    HashSet<Integer> rootInd;
 
     public djSet(int x) {
         set = new int[x + 1];
         size = new int[x + 1];
+        rootInd = new HashSet<>();
         //makes each set at least contain itself
         for (int i = 0; i < set.length; i++) {
             set[i] = i;
@@ -60,13 +76,10 @@ class djSet {
     }
 
     //adds string of two sets to each other
-    public void union(String s) {
+    public void union(connection c) {
         //parses string into two sets
-        String[] sets = s.split(" ");
-        int set1 = Integer.parseInt(sets[0]);
-        int set2 = Integer.parseInt(sets[1]);
-        int root1 = findSet(set1);
-        int root2 = findSet(set2);
+        int root1 = findSet(c.c1);
+        int root2 = findSet(c.c2);
 
         if (root1 == root2) return;
 
@@ -74,6 +87,8 @@ class djSet {
         set[root2] = root1;
         size[(int) root1] += size[(int) root2];
         size[(int) root2] = 0;
+        rootInd.add(root1);
+        rootInd.remove((Object) root2);
     }
 
     //returns the root of the set working with, and does path compression on the way out for speed ups later
@@ -91,11 +106,16 @@ class djSet {
     //finds the connectivity by going through the array and groups similar roots together. At end squares each similar root total and sums it
     public long findConnectivity() {
         long sum = 0;
-        for (int x = 0; x < size.length; x++) {
-            long hold = size[x];
+        //I make the total amount of this the size-1, which is equal to the amount of actual nodes the set can work with
+        long tot = size.length - 1;
+        Iterator I = rootInd.iterator();
+        while (I.hasNext()) {
+            long hold = size[(int) I.next()];
             sum += (hold * hold);
+            tot -= hold;
         }
-        return sum;
+        //this works because the total will hold all the size 1 arrays, and 1^2 = 1
+        return (sum + tot);
     }
 
     //prints out the set (testing purposes)
